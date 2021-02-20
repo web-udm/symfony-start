b0c7dc2f4faefb0c551baaa3fe2a7e21ec216910
moildar <i.mosnabeev@taxi-master.com>
2021-02-20 17:20:27 +0400

    Добавил опасный контроллер

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,5 +19,15 @@ class ConferenceController extends AbstractController
         ]);*/
 
         return new Response("<img src='img/website-under-construction-image-6.gif'>");
+    }
+
+    /**
+     * @Route("/hello", name="hello")
+     */
+    public function hello(Request $request): Response
+    {
+        $name = $request->query->get('name');
+
+        return new Response("Hello, $name");
     }
 }
