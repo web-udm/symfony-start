f3eb3bd68fb53c35b6ccfd2f90c25c10336a36bb
moildar <i.mosnabeev@taxi-master.com>
2021-02-24 19:05:16 +0400

    Добавил отрисовку отдельной страницы под конференции

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,34 +2,40 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
+use App\Entity\Conference;
 
 class ConferenceController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
+     * @param Environment $twig
      * @param ConferenceRepository $conferenceRepository
      * @return Response
      */
-    public function index(ConferenceRepository $conferenceRepository): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        return $this->render('conference/index.html.twig', [
+        return new Response($twig->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
-        ]);
+        ]));
     }
 
     /**
-     * @Route("/hello", name="hello")
+     * @Route("/conference/{id}", name="conference")
+     * @param Environment $twig
+     * @param Conference $conference
+     * @param CommentRepository
      */
-    public function hello(Request $request): Response
+    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
     {
-        $name = $request->query->get('name');
-
-        return new Response("Hello, $name");
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $commentRepository->findBy(['conference' => $conference], ['createAt' => 'DESC']),
+        ]));
     }
 }
