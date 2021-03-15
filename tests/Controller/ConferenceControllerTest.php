012d67b2c1fb6e1b51282cc8837486df887ffbf5
moildar <i.mosnabeev@taxi-master.com>
2021-03-15 12:32:53 +0400

    Написал тест для контроллера

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -14,4 +14,19 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
     }
+
+    public function testConferencePage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink('Amsterdam in 2019');
+
+        $this->assertPageTitleContains('Amsterdam');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Amsterdam in 2019 Conference');
+        $this->assertSelectorExists('div:contains("There are 1 comments")');
+    }
 }
