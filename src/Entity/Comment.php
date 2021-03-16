fcc12898bee120df208786b368cac82a604e4816
moildar <i.mosnabeev@taxi-master.com>
2021-03-16 12:01:46 +0400

    Добавил свойство state в Comment

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -54,6 +54,11 @@ class Comment
      */
     private $photoFilename;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $state;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -142,5 +147,17 @@ class Comment
     public function setCreatedAtValue()
     {
         $this->createAt = new \DateTime();
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): self
+    {
+        $this->state = $state;
+
+        return $this;
     }
 }
